548a5104bff379235785e629edb28042f5a9c89b
Daniel Martinez <roanhawkeye@gmail.com>
2022-03-31 10:21:51 -0600

    Create Accordion component with sample data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library amon engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
 export default () => {
-  return <h1>Widget App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
---
+++ src/components/Accordion.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Accordion = ({ items }) => {
+  return <h1>{items.length}</h1>;
+};
+
+export default Accordion;
