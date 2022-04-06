ba5e069ef47e9f5cbceaf7f5e8e4e9e686410b5b
Daniel Martinez <roanhawkeye@gmail.com>
2022-04-06 17:34:58 -0500

    Get results from wikipedia API

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
-  console.log("I run with every render!");
+  const renderedResults = results.map((result) => (
+    <div key={result.pageid} className="item">
+      <div className="content">
+        <div className="header">{result.title}</div>
+        {result.snippet}
+      </div>
+    </div>
+  ));
 
   useEffect(() => {
-    console.log("I run at every re-render");
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    search();
   }, [term]);
 
   return (
@@ -21,6 +44,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
