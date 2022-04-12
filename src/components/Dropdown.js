350dcf77eeb8f30bf60839dbe6a6176f7e7d8f82
Daniel Martinez <roanhawkeye@gmail.com>
2022-04-11 20:49:01 -0500

    Create Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => (
+    <div key={option.value} className="item">
+      {option.label}
+    </div>
+  ));
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="labe">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
