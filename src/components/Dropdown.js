44a4299865273abdbd0fd42eeed0cc522da92fe1
Daniel Martinez <roanhawkeye@gmail.com>
2022-04-17 15:32:08 -0500

    Toggle dropdown in main page

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,12 +5,20 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener("click", (event) => {
+    const onBodyClick = (event) => {
       if (ref.current.contains(event.target)) {
         return;
       }
       setOpen(false);
-    });
+    };
+
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
   }, []);
 
   const renderedOptions = options.map((option) => {
